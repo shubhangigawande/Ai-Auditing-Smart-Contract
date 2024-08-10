ee934532a67ebbf19fc2ca8147c709863763f4ac
ygsdyu jhdbhj <shubhangigawande121@gmail.com>
2024-08-10 11:56:58 +0530

    upload files

---
+++ src/demo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Demo() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-3xl">Hello, Tailwind CSS with React & TypeScript!</h1>
+    </div>
+  );
+}
+
